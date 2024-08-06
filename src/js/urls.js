7face84bdd70abfaefd3720551ec9e482f10ddfb
Felipe Santana <felipe012santana012@gmail.com>
2024-08-06 19:44:23 -0300

    alteração url back railway

--- src/js/urls.js
+++ src/js/urls.js
@@ -1,6 +1,10 @@
 // const url = "https://back-end-mania-food.vercel.app/";
-const url = "https://backend-mania-food.onrender.com";
+
+// const url = "https://backend-mania-food.onrender.com";
+
 // const url =
 //   "https://my-json-server.typicode.com/felipesantana012/api-json-mania-food/";
 
 // const url = "http://localhost:3000/";
+
+const url = "https://backend-mania-food-production.up.railway.app";
